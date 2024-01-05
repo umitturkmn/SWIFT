3bdae144d654052f832db8f0ab2d1a41d26ad3e9
umitturkmn <umit.482@gmail.com>
2024-01-05 17:27:35 +0300

    FaceRecognit cart curt is completed

--- FaceRecognitionApp/FaceRecognitionApp/ViewController.swift
+++ FaceRecognitionApp/FaceRecognitionApp/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        let authContext = LAContext()
+        var error: NSError?
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Bu sen misin?") { success, error in
+                if success == true{
+                    DispatchQueue.main.async{
+                        self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                    }
+                } else {
+                    DispatchQueue.main.async{
+                        self.myLabel.text = "Hatalı Giriş"
+                    }
+                    
+                }
+            }
+        }
+    }
+    
 }
 
