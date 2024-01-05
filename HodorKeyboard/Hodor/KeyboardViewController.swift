f038c09cdb0fa6f12a04c688329235441065c630
umitturkmn <umit.482@gmail.com>
2024-01-05 17:47:03 +0300

    hodor is completed

---
+++ HodorKeyboard/Hodor/KeyboardViewController.swift
@@ -0,0 +1,72 @@
+//
+//  KeyboardViewController.swift
+//  Hodor
+//
+//  Created by Ümit Türkmen on 5.01.2024.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        
+        // Add custom view sizing constraints here
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Hodor
+        
+        let hodorButton = UIButton(type: UIButton.ButtonType.system)
+        hodorButton.frame = CGRect(x: 50, y: 50, width: 50, height: 50)
+        hodorButton.setBackgroundImage(UIImage(named: "hodor.jpg"), for: UIControl.State.normal)
+        hodorButton.addTarget(self, action: #selector(honorTapped), for: UIControl.Event.touchUpInside)
+        view.addSubview(hodorButton)
+        
+        
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton(type: .system)
+        
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+        
+        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+    @objc func honorTapped(){
+        let textProxy = textDocumentProxy as UITextDocumentProxy
+        textProxy.insertText("hodor")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        self.nextKeyboardButton.isHidden = !self.needsInputModeSwitchKey
+        super.viewWillLayoutSubviews()
+    }
+    
+    override func textWillChange(_ textInput: UITextInput?) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+    
+    override func textDidChange(_ textInput: UITextInput?) {
+        // The app has just changed the document's contents, the document context has been updated.
+        
+        var textColor: UIColor
+        let proxy = self.textDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
+            textColor = UIColor.white
+        } else {
+            textColor = UIColor.black
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, for: [])
+    }
+
+}
