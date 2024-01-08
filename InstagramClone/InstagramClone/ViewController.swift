9b6d726f20276fffb4a2249e68e057af1674be2c
umitturkmn <umit.482@gmail.com>
2024-01-08 20:40:16 +0300

    InstagramClone app is created

---
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Ümit Türkmen on 8.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
