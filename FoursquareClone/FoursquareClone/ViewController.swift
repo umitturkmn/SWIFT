1b52e019803cac54b4ea587a6346bce7ba558478
umitturkmn <umit.482@gmail.com>
2024-01-10 20:24:37 +0300

    Foursquare Clone is created

---
+++ FoursquareClone/FoursquareClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by Ümit Türkmen on 10.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
