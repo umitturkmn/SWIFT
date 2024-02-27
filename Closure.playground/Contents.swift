38f98991cee04e9eec98a8a23eac50ccdc794099
umitturkmn <umit.482@gmail.com>
2024-02-27 22:07:14 +0300

    Boring...

--- Closure.playground/Contents.swift
+++ Closure.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+func sum(num1: Int, num2: Int) -> Int {
+    return num1 + num2
+}
+
+sum(num1: 4, num2: 654)
+
+
+
+
+func calculete(num1: Int, num2: Int, sumFunc: (Int,Int) -> Int) -> Int {
+    return sumFunc(num1,num2)
+}
+
+calculete(num1: 23, num2: 3445, sumFunc : sum)
+
+/*
+{ (num1: Int, num2: Int) -> Int in
+    return num1 * num2
+}
+*/
+
