f594e4e70d69f72863cb9bf8acc9341d5bc770e2
umitturkmn <umit.482@gmail.com>
2024-02-29 17:44:10 +0300

    Project is created.

---
+++ FirstSwiftUIProject/FirstSwiftUIProject/FirstSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIProjectApp.swift
+//  FirstSwiftUIProject
+//
+//  Created by Ümit Türkmen on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
