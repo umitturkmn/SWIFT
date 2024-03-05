d4b4ad941f972cc2c6ffeca862eec2b5dc65eb83
umitturkmn <umit.482@gmail.com>
2024-03-05 17:18:02 +0300

    Project is created...

---
+++ WidgetHero/WidgetHero/WidgetHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetHeroApp.swift
+//  WidgetHero
+//
+//  Created by Ümit Türkmen on 5.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WidgetHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
