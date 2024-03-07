6352d24b243e3c76c41a687327b074dde9297e25
umitturkmn <umit.482@gmail.com>
2024-03-07 17:49:56 +0300

    Project is Created

---
+++ ThreadingProject/ThreadingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreadingProject
+//
+//  Created by Ümit Türkmen on 7.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
