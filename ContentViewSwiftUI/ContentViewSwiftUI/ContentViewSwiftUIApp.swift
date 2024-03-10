fd9d863ab46ee08abbea7f374e6285cc2633ac5e
umitturkmn <umit.482@gmail.com>
2024-03-11 02:56:06 +0300

    Project is Created

---
+++ ContentViewSwiftUI/ContentViewSwiftUI/ContentViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentViewSwiftUIApp.swift
+//  ContentViewSwiftUI
+//
+//  Created by Ümit Türkmen on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContentViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
