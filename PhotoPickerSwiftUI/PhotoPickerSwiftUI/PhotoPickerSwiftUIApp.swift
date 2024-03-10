e2e047c32be5f3a3a35948c3ce2c0fe2af02422b
umitturkmn <umit.482@gmail.com>
2024-03-11 02:15:34 +0300

    Project is created.

---
+++ PhotoPickerSwiftUI/PhotoPickerSwiftUI/PhotoPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerSwiftUIApp.swift
+//  PhotoPickerSwiftUI
+//
+//  Created by Ümit Türkmen on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
