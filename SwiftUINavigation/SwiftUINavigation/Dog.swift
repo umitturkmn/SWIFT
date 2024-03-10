d514849ec6de281f0e6207bcba31d8761d10b6d7
umitturkmn <umit.482@gmail.com>
2024-03-11 01:53:46 +0300

    Project is created.

---
+++ SwiftUINavigation/SwiftUINavigation/Dog.swift
@@ -0,0 +1,16 @@
+//
+//  Dog.swift
+//  SwiftUINavigation
+//
+//  Created by Ümit Türkmen on 11.03.2024.
+//
+
+import Foundation
+
+struct Dog: Identifiable, Hashable {
+    var id = UUID()
+    let name : String
+    
+}
+
+
---
+++ SwiftUINavigation/SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Ümit Türkmen on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
