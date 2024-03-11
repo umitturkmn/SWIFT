2604d131d186ea558e337e29aacffbd1fa0f533b
umitturkmn <umit.482@gmail.com>
2024-03-11 17:32:31 +0300

    Project is created.

---
+++ ToDoListSWIFT/ToDoListSWIFT/ToDoListSWIFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSWIFTApp.swift
+//  ToDoListSWIFT
+//
+//  Created by Ümit Türkmen on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSWIFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
