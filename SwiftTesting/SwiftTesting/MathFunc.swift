314a272fbd89c60695b26ef5862d12240cac35d3
umitturkmn <umit.482@gmail.com>
2024-03-15 14:07:31 +0300

    Project is completed.

---
+++ SwiftTesting/SwiftTesting/MathFunc.swift
@@ -0,0 +1,21 @@
+//
+//  MathFunc.swift
+//  SwiftTesting
+//
+//  Created by Ümit Türkmen on 15.03.2024.
+//
+
+import Foundation
+
+class MathFunc{
+    
+    func sum(x:Int,y:Int) -> Int{
+        return x + y
+    }
+    func multi(x:Int,y:Int) -> Int{
+        return x * y
+    }
+    func divide(x:Int,y:Int) -> Int{
+        return x / y
+    }
+}
