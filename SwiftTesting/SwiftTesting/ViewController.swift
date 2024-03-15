48793d740e4c9b7577d57cefbf3e064b240a201c
umitturkmn <umit.482@gmail.com>
2024-03-15 13:57:07 +0300

    Project is created

---
+++ SwiftTesting/SwiftTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftTesting
+//
+//  Created by Ümit Türkmen on 15.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
