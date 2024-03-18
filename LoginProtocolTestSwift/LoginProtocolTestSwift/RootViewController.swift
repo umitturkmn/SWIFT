ada9fa0ea40620bd9366acdc0bc54925e4cb48ab
umitturkmn <umit.482@gmail.com>
2024-03-18 16:07:18 +0300

    Project is Created

---
+++ LoginProtocolTestSwift/LoginProtocolTestSwift/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoginProtocolTestSwift
+//
+//  Created by Ümit Türkmen on 18.03.2024.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
