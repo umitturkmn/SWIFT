21dd9c91f7770494059475315256b15fbd0d673e
umitturkmn <umit.482@gmail.com>
2024-03-18 21:11:43 +0300

    Project Completed.

--- GoogleAdMobTest/GoogleAdMobTest/ViewController.swift
+++ GoogleAdMobTest/GoogleAdMobTest/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , GADFullScreenContentDelegate {
+    
+    private var interstitial: GADInterstitialAd?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        let request = GADRequest()
+            GADInterstitialAd.load(withAdUnitID: "ca-app-pub-3940256099942544/4411468910",
+                                        request: request,
+                              completionHandler: { [self] ad, error in
+                                if let error = error {
+                                  print("Failed to load interstitial ad with error: \(error.localizedDescription)")
+                                  return
+                                }
+                                interstitial = ad
+                                interstitial?.fullScreenContentDelegate = self
+                              }
+                                )
+        
+        
     }
+    /// Tells the delegate that the ad failed to present full screen content.
+     func ad(_ ad: GADFullScreenPresentingAd, didFailToPresentFullScreenContentWithError error: Error) {
+       print("Ad did fail to present full screen content.")
+     }
 
+     /// Tells the delegate that the ad will present full screen content.
+     func adWillPresentFullScreenContent(_ ad: GADFullScreenPresentingAd) {
+       print("Ad will present full screen content.")
+     }
 
+     /// Tells the delegate that the ad dismissed full screen content.
+     func adDidDismissFullScreenContent(_ ad: GADFullScreenPresentingAd) {
+       print("Ad did dismiss full screen content.")
+     }
+
+    @IBAction func nextClicked(_ sender: Any) {
+        
+        if interstitial != nil {
+            interstitial!.present(fromRootViewController: self)
+          } else {
+            print("Ad wasn't ready")
+          }
+        
+    }
 }
 
---
+++ GoogleAdMobTest/Pods/Target Support Files/Pods-GoogleAdMobTestTests/Pods-GoogleAdMobTestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoogleAdMobTestTests : NSObject
+@end
+@implementation PodsDummy_Pods_GoogleAdMobTestTests
+@end
